7f93e36413de621e7bcf9417c6181ada7138ba5c
asatake <asataken@gmail.com>
2019-02-12 14:04:08 +0900

    :sparkles: init commit

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "../assets/App.css";
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <p>test</p>
+      </div>
+    );
+  }
+}
+
+export default App;
